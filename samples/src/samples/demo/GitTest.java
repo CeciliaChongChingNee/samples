3254fc0e2f05b9b299ec49082cea41c1ed13f4bd
CeciliaChongChingNee <b031910390@student.utem.edu.my>
2021-03-18 13:33:22 +0800

    Initial Commit

---
+++ samples/src/samples/demo/GitTest.java
@@ -0,0 +1,24 @@
+/**
+ * Create a new package named samples.demo
+ */
+package samples.demo;
+
+/**
+ * Create a new class named GitTest
+ * @author ching
+ *
+ */
+public class GitTest {
+
+	/**
+	 * Main entry point of program
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//Display output
+		System.out.println("Git is fun");
+
+	}
+
+}
