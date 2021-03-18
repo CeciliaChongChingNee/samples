70c88dc71523866013f7f60f709eb372cbb5ab57
CeciliaChongChingNee <b031910390@student.utem.edu.my>
2021-03-18 13:40:20 +0800

    Add Location class

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,30 @@
+/**
+ * Create a new package named samples.demo
+ */
+package samples.demo;
+
+/**
+ * Create a new class named Location
+ * @author ching
+ *
+ */
+public class Location {
+	
+	//attribute of this class
+	private String name;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
